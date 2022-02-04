c1851005f231678bad99c89ef6f1510b02afa1c1
amirreza sokhankhosh <amirrezasokhankhosh79@gmail.com>
2022-02-04 09:41:37 +0330

    Version 1.0.1 (Names Updated.)

--- main.py
+++ main.py
@@ -1,8 +1,31 @@
+from statistics import variance
+
+
 class Student:
     def __init__(self, s_id, name):
         self.s_id = s_id
         self.name = name
         self.assignments = {'hw1': 0, 'hw2': 0, 'p1': 0, 'p2': 0}
+
+
+def add_scores(file, assignment, students):
+    for line in file.readlines():
+        variables = line.split()
+        score = variables[-1]
+        s_id = variables[-2]
+        name = ''
+        for i in range(len(variables) - 2):
+            name = name + ' ' + variables[i] 
+        if s_id not in students.keys():
+            student = Student(s_id, name)
+            student.assignments[assignment] = score
+            students[s_id] = student
+        else:
+            student = students[s_id]
+            student.assignments[assignment] = score
+            students[s_id] = student
+    
+    return students 
 
 
 hw1_f = open('./hw1.txt', 'r')
@@ -13,51 +36,10 @@ p2_f = open('./p2.txt', 'r')
 
 students = {}
 
-for line in hw1_f.readlines():
-    fname, lname, s_id, score = line.split()
-    if s_id not in students.keys():
-        student = Student(s_id, fname + ' ' + lname)
-        student.assignments['hw1'] = score
-        students[s_id] = student
-    else:
-        student = students[s_id]
-        student.assignments['hw1'] = score
-        students[s_id] = student
-
-
-for line in hw2_f.readlines():
-    fname, lname, s_id, score = line.split()
-    if s_id not in students.keys():
-        student = Student(s_id, fname + ' ' + lname)
-        student.assignments['hw2'] = score
-        students[s_id] = student
-    else:
-        student = students[s_id]
-        student.assignments['hw2'] = score
-        students[s_id] = student
-
-for line in p1_f.readlines():
-    fname, lname, s_id, score = line.split()
-    if s_id not in students.keys():
-        student = Student(s_id, fname + ' ' + lname)
-        student.assignments['p1'] = score
-        students[s_id] = student
-    else:
-        student = students[s_id]
-        student.assignments['p1'] = score
-        students[s_id] = student
-
-for line in p2_f.readlines():
-    fname, lname, s_id, score = line.split()
-    if s_id not in students.keys():
-        student = Student(s_id, fname + ' ' + lname)
-        student.assignments['p2'] = score
-        students[s_id] = student
-    else:
-        student = students[s_id]
-        student.assignments['p2'] = score
-        students[s_id] = student
-
+students = add_scores(hw1_f, 'hw1', students)
+students = add_scores(hw2_f, 'hw2', students)
+students = add_scores(p1_f, 'p1', students)
+students = add_scores(p2_f, 'p2', students)
 
 ta_f = open('./TA.csv', 'w')
 for s_id in students.keys():
